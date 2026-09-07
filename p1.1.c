21d828a57587f256a577e52d511f211b89a90c28
aryan mishra <aryanmishra876731@gmail.com>
2026-09-07 19:34:44 +0530

    p1.1.c

---
+++ p1.1.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+
+void selectionSort(int A[], int n)
+{
+    int i, j, min, temp;
+
+    for(i = 0; i < n - 1; i++)
+    {
+        min = i;
+
+        for(j = i + 1; j < n; j++)
+        {
+            if(A[j] < A[min])
+            {
+                min = j;
+            }
+        }
+
+        if(min != i)
+        {
+            temp = A[i];
+            A[i] = A[min];
+            A[min] = temp;
+        }
+    }
+}
+
+int main()
+{
+    int A[100], n, i;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter %d elements:\n", n);
+    for(i = 0; i < n; i++)
+    {
+        scanf("%d", &A[i]);
+    }
+
+    selectionSort(A, n);
+
+    printf("\nSorted array:\n");
+    for(i = 0; i < n; i++)
+    {
+        printf("%d ", A[i]);
+    }
+
+    return 0;
+}
